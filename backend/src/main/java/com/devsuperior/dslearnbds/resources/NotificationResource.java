03d242d0d19d83d2ca313ff0440d19b5a1c8265f
ValberMedeiros <dev.junior.medeiros@gmail.com>
2021-06-27 20:28:07 -0300

    Buscar de notificações do usuário logado.

---
+++ backend/src/main/java/com/devsuperior/dslearnbds/resources/NotificationResource.java
@@ -0,0 +1,26 @@
+package com.devsuperior.dslearnbds.resources;
+
+import com.devsuperior.dslearnbds.dto.NotificationDTO;
+import com.devsuperior.dslearnbds.services.NotificationService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/notifications")
+public class NotificationResource {
+
+    private NotificationService service;
+
+    public NotificationResource(NotificationService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<NotificationDTO>> notificationForCurrentUser(Pageable pageable) {
+        return ResponseEntity.ok(service.notificationsForCurrentUser(pageable));
+    }
+}
---
+++ backend/src/main/java/com/devsuperior/dslearnbds/services/NotificationService.java
@@ -0,0 +1,29 @@
+package com.devsuperior.dslearnbds.services;
+
+import com.devsuperior.dslearnbds.dto.NotificationDTO;
+import com.devsuperior.dslearnbds.entities.Notification;
+import com.devsuperior.dslearnbds.repositories.NotificationRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class NotificationService {
+
+    private NotificationRepository repository;
+    private AuthService authService;
+
+    public NotificationService(NotificationRepository repository, AuthService authService) {
+        this.repository = repository;
+        this.authService = authService;
+    }
+
+    @Transactional(readOnly = true)
+    public Page<NotificationDTO> notificationsForCurrentUser(Pageable pageable) {
+        var user = authService.authenticated();
+        Page<Notification> page = repository.findByUser(user, pageable);
+        return page.map(NotificationDTO::new);
+    }
+
+}
